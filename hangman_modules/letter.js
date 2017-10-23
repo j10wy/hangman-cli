41f2ea41861482cff133b2ecb78bf07c0385e3c4
Jeffrey Lowy <jeffreylowy@gmail.com>
2017-10-23 11:52:10 -0700

    Separate functions in letter.js

--- hangman_modules/letter.js
+++ hangman_modules/letter.js
@@ -6,30 +6,9 @@ function Letter(letter, word) {
 	// Log function to help with debugging
 	this.log = log;
 	// Method to determine if the letter is in the word array-like object;
-	this.inArray = (word, letter) => {
-		word.find(entry => {
-			return entry === letter;
-		});
-	}
+	this.inArray = inArray
 	// The validate method will determine if the user can proceed to Inquirer's then call
-	this.validate = () => {
-		// Create a regex to test if entry is a digit
-		var regexNum = /\d/;
-
-		if (regexNum.exec(this.letter)) {
-			// Test for digits first. 
-			this.log("\n\nNumbers not allowed!\n");
-			return false;
-		} else if (this.letter.length > 1 || this.letter.length === 0) {
-			// if value is not a digit, text if the length is lt 0 or gt 1
-			this.log("\n\nEnter a single letter\n");
-			return false;
-		} else {
-			// Letter seems to be a valid entry
-			this.log("Valid entry...\n");
-			return true;
-		}
-	}
+	this.validate = validate;
 }
 
 function log(stuffToLog) {
@@ -40,5 +19,30 @@ function log(stuffToLog) {
 	// Example: return new Letter(value, word).log("Logging stuff...").validate();
 	return this;
 };
+
+function inArray(word, letter) {
+	word.find(entry => {
+		return entry === letter;
+	});
+}
+
+function validate() {
+	// Create a regex to test if entry is a digit
+	var regexNum = /\d/;
+
+	if (regexNum.exec(this.letter)) {
+		// Test for digits first. 
+		this.log("\n\nNumbers not allowed!\n");
+		return false;
+	} else if (this.letter.length > 1 || this.letter.length === 0) {
+		// if value is not a digit, text if the length is lt 0 or gt 1
+		this.log("\n\nEnter a single letter\n");
+		return false;
+	} else {
+		// Letter seems to be a valid entry
+		this.log("Valid entry...\n");
+		return true;
+	}
+}
 
 module.exports = Letter;
