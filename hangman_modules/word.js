bdefbad8044ef2f9195fbe53ab9235182f4c920d
Jeffrey Lowy <jeffreylowy@gmail.com>
2017-10-23 14:17:09 -0700

    Added Word.displayWord() to display number of dashes.

--- hangman_modules/word.js
+++ hangman_modules/word.js
@@ -1,19 +1,30 @@
 var words = require("./words");
 
 function Word() {
-	
+
 	this.word = randomWord(words);
-	this.log = () => {
-		console.log(this.word);
-	}
+	this.displayWord = displayWord;
 
 }
 
-function randomWord (arr) {
-    var numberOfLetters = arr.length - 1;
-    var min = Math.ceil(0);
-    var max = Math.floor(numberOfLetters);
-    return arr[Math.floor(Math.random() * (max - min + 1)) + min];
+// FUNCTIONS
+
+function displayWord() {
+	var length = this.word.length;
+	var dashes = "";
+	(function __(numberOfDashes) {
+		for (var _ = 0; _ < length; _++) {
+			dashes += "_";
+		}
+	}(length));
+	console.log(dashes);
+}
+
+function randomWord(arr) {
+	var numberOfLetters = arr.length - 1;
+	var min = Math.ceil(0);
+	var max = Math.floor(numberOfLetters);
+	return arr[Math.floor(Math.random() * (max - min + 1)) + min];
 }
 
 module.exports = Word;
