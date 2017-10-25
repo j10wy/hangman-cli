6d6b88f903d51f785114ab0bc600d2d2c01b27d0
Jeffrey Lowy <jeffreylowy@gmail.com>
2017-10-25 00:16:38 -0700

    use const / gen. clean-up

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
 	@body Use require package to colorize the terminal output
 */
 
-let Word = require("./hangman_modules/word");
-let Letter = require("./hangman_modules/letter");
-var inquirer = require('inquirer');
+const Word = require("./hangman_modules/word");
+const Letter = require("./hangman_modules/letter");
 
+var inquirer = require('inquirer');
 var word = null;
 
 function startGame() {
@@ -15,12 +15,11 @@ function startGame() {
 	inquirer.prompt([{
 		type: 'input',
 		name: 'letter',
-		message: "Enter a letter",
+		message: "Guess a letter",
 		validate: function (value) {
 			return new Letter(value, word).validate();
 		}
 	}]).then(() => {
-		console.log("\n\nYou win!\n\n".toUpperCase());
 		playAgain();
 	});
 }
